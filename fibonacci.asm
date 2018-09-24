c5b083db9086935a09cf04dcfa132ee23b68c6a1
arywd5 <31825610+arywd5@users.noreply.github.com>
2018-09-24 16:42:37 -0500

    Fibonacci code finished with numbers in decimal

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,13 +1,14 @@
 #this program uses the value stored in register t0 
     #and calculated the Fibonaccci number from it 
-        addi $a0, $t0, 0x0000       #store the number in a0 = 10
-        andi $a1, $a1, 0x0000       #set a1 to 0 so we can have a counter 
-        andi $t1, $t1, 0x0000       #clear t0 register to hold our value 
-        andi $t2, t2, 0x00001       #F1 of fibonacci sequence 
-        andi $t3, %t3, 0x0001       #F2 of fibonocci sequence 
-        beq $a0, $a1, DONE          #if the counter is equal to the jump to end 
-        add $t1, $t1, $t2           #add the previous number of sequence
-
-
-
-DONE:   #answer in t0
+        addi $a0, $t0, 0     ;0x0000 
+		andi $a1, $a1, 3     ;0x0004
+        andi $t1, $t1, 1     ;0x0008
+        andi $t2, $t2, 1     ;0x000c 12   
+        andi $t3, $t3, 1     ;0x000F 16 
+        beq $a0, $a1, 44     ;0x0014 20  
+        add $t1, $t2, $t3    ;0x0018 24 
+		and $t2, $t3, $t3	 ;0x001C 28		
+		and $t3, $t1, $t1	 ;0x001F 32
+		addi $a1, $a1, 1	 ;0x0024 36	
+		j 20				 ;0x0028 40
+		syscall 			 ;0x002C 44
